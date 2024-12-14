ab0947406c06ab60c0e5f51b8802ebc630a68311
Sigma! <103079800+RizynzSchool@users.noreply.github.com>
2024-12-14 17:40:58 -0600

    Update run.bash
    BrainF-Compiler-V2

--- run.bash
+++ run.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-cd /workspaces/codespaces-blank/src
-python compiler.py
-g++ compiled.cpp
 clear
+cd /workspaces/codespaces-blank/src 
+
+# run compiler and compiled cpp
+python compiler.py 
+g++ compiled.cpp 
+
 ./a.out
+
+# delete cpp and compiled file after execution
+rm compiled.cpp
+rm a.out 
